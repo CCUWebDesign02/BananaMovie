c422383589b69581b1fc6f2552aa9288eb06a1ed
Vincent <vincent0740@gmail.com>
2018-01-08 00:12:55 +0800

    change ini

--- environment.php
+++ environment.php
@@ -4,3 +4,5 @@ $config = parse_ini_file('config.ini');
 define('BASE_PATH', __DIR__);
 define('SR_PASSLENGTH', $config['SR_PASSLENGTH']);
 define('TIMEZONE', $config['TIMEZONE']);
+define('ERROR_DISPLAY', $config['ERROR_DISPLAY']);
+error_reporting(ERROR_DISPLAY);
