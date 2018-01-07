8df38b831ec2555e733f2cac567fd04ef7737d57
Vincent <vincent0740@gmail.com>
2018-01-07 18:14:10 +0800

    movie_intro finished(no data)

--- views/movie_intro.view.php
+++ views/movie_intro.view.php
@@ -1,60 +1,122 @@
-<div class="container inproduce">
-    <div class="buying">
-        <div class="row">
-            <div class="col-md-4">
-                <img id="mimgs" src="imgs/movie/coco-poster.jpg">
-            </div>
-            <div class="col-md-4">
-                <div class="row">
-                    <div class="tm">
-                        <h2>可可夜總會<br>Coco</h2>
-                    </div>&nbsp;&nbsp;&nbsp;<img id="warning" src="imgs/普遍級.png">
+<div class="container introduce">
+    <div class="row buying p-4 mb-4">
+        <div class="col-md-1"></div>
+        <div class="col-md-3 align-self-center">
+            <img id="mimgs" src="imgs/movie/coco-poster.jpg">
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-6">
+            <div class="row movie-title align-items-center mb-2">
+                <div>
+                    <div class="zh">可可夜總會</div>
+                    <div class="en">Coco</div>
                 </div>
-                <br>
-                <div class="timechartt">
-                    <h3>&nbsp;時刻表</h3></div>
-                <div class="timechartb">
-                    <h4>1/12<br><br>&nbsp;5廳<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10:40&nbsp;&nbsp;&nbsp;14:40&nbsp;&nbsp;&nbsp;18:40&nbsp;&nbsp;&nbsp;22:40&nbsp;&nbsp;&nbsp;</h4>
-                    <hr class="margin-top-0">
-                    <h4>1/14<br><br>&nbsp;5廳<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10:40&nbsp;&nbsp;&nbsp;14:40&nbsp;&nbsp;&nbsp;18:40&nbsp;&nbsp;&nbsp;22:40&nbsp;&nbsp;&nbsp;</h4>
+                <img class="my-1 mx-3" src="imgs/rating-0.png">
+            </div>
+            <div class="row">
+                <div class="timechartt px-2">時刻表</div>
+                <div class="timechartb p-4">
+                    <div class="date">1/12</div>
+                    <div class="d-flex">
+                        <div class="hall mr-5">5廳</div>
+                        <div class="time mr-4">10:40</div>
+                        <div class="time mr-4">14:40</div>
+                        <div class="time mr-4">18:40</div>
+                        <div class="time mr-4">22:40</div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="hall mr-5">6廳</div>
+                        <div class="time mr-4">10:40</div>
+                        <div class="time mr-4">14:40</div>
+                        <div class="time mr-4">18:40</div>
+                        <div class="time mr-4">22:40</div>
+                    </div>
+                    <hr class="my-4">
+                    <div class="date">1/12</div>
+                    <div class="d-flex">
+                        <div class="hall mr-5">5廳</div>
+                        <div class="time mr-4">10:40</div>
+                        <div class="time mr-4">14:40</div>
+                        <div class="time mr-4">18:40</div>
+                        <div class="time mr-4">22:40</div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="hall mr-5">6廳</div>
+                        <div class="time mr-4">10:40</div>
+                        <div class="time mr-4">14:40</div>
+                        <div class="time mr-4">18:40</div>
+                        <div class="time mr-4">22:40</div>
+                    </div>
                 </div>
             </div>
         </div>
-        <br>
-        <h2>立即購票</h2>
-        <br>
-        <div class="row">
-            <h3>日期: <select><option value="2018/1/12">2018/1/12</option><option value="2018/1/12">2018/1/14</option></select>&nbsp;&nbsp;&nbsp; 
-             廳: <select><option value="place5">5廳</option></select>&nbsp;&nbsp;&nbsp; 
-             場次: <select><option value="10:40">10:40</option><option value="14:40">14:40</option><option value="18:40">18:40</option><option value="22:40">22:40</option></select>&nbsp;&nbsp;&nbsp; 
-             張數: <select><option value="ticket1">1</option><option value="ticket2">2</option><option value="ticket3">3</option><option value="ticket4">4</option><option value="ticket5">5</option></select>&nbsp;&nbsp;&nbsp; </h3>
-        </div>
-        <div class="row" style="float:right;">
-            <input type="button" style="font-size:20px;background-color:#ec0d3e;color:white;width:120px;height:50px;" value="立即結帳">&nbsp;&nbsp;&nbsp;
-            <input type="button" style="font-size:20px;background-color:#0092c7;color:white;width:120px;height:50px;" value="加入購物車">&nbsp;&nbsp;&nbsp;
-            <h1>520元</h1></div>
-        <br>
-    </div>
-    <div class="mdetails">
-        <div class="row">
-            <div class="col-md-6">
-                <h2 align="left" style="line-height: 40px">電影簡介</h2>
-                <br>
-                <h4 align="left" style="line-height: 40px">&nbsp;日期&nbsp;&nbsp;2017/12/27</h4>
-                <h4 align="left" style="line-height: 40px">&nbsp;導演&nbsp;&nbsp;李·安克里奇(Lee Unkrich)</h4>
-                <h4 align="left" style="line-height: 40px">&nbsp;演員&nbsp;&nbsp;&nbsp;蓋爾·賈西亞·貝納(Gael García Bernal) <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;本杰明·布拉特(Benjamin Bratt) 
-            <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;芮妮·维克托(Renée Victor)</h4>
+        <div class="col-md-1"></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-10 buy-select py-4">
+            <div class="row">
+                <div class="pb-2">立即購票</div>
+                <div class="w-100"></div>
+                <div class="px-1">日期:</div>
+                <select class="px-2 ml-1 mr-4">
+                    <option value="2018/1/12">2018/1/12</option>
+                    <option value="2018/1/12">2018/1/14</option>
+                </select>
+                <div>廳:</div>
+                <select class="px-2 ml-1 mr-4">
+                    <option value="place5">5廳</option>
+                </select>
+                <div>場次:</div>
+                <select class="px-2 ml-1 mr-4">
+                    <option value="10:40">10:40</option>
+                    <option value="14:40">14:40</option>
+                    <option value="18:40">18:40</option>
+                    <option value="22:40">22:40</option>
+                </select>
+                <div>張數:</div>
+                <select class="px-2 ml-1 mr-4">
+                    <option value="ticket1">1</option>
+                    <option value="ticket2">2</option>
+                    <option value="ticket3">3</option>
+                    <option value="ticket4">4</option>
+                    <option value="ticket5">5</option>
+                </select>
             </div>
-            <div class="col-md-6">
-                <h2 align="left" style="line-height: 40px">電影預告</h2>
-                <br>
-                <iframe width="490px" height="300px" src="https://www.youtube.com/embed/JmSU0ZjjjXM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
-            </div>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-6"></div>
+        <div class="col-md-6 buy-confirm d-flex justify-content-around align-items-center">
+            <button>立即結帳</button>
+            <button>加入購物車</button>
+            <div id="price">520元</div>
         </div>
     </div>
-    <div class="story">
-        <h2 align="left" style="line-height: 40px">劇情介紹</h2>
-        <br>
-        <h3 style="line-height: 40px;">《可可夜總會》故事描述墨西哥的鞋匠家族「里韋拉斯」有個代代相傳的家訓，就是家族的成員絕不能與音樂有任何關係，但天生音樂魂的男孩米高難以抵擋音樂的魅力，時常躲在家中閣樓練習，更無師自通了一身不得了的吉他技巧。就在「亡靈節」這天，父親發現了充滿樂譜的秘密閣樓，盛怒之下砸了他的吉他，傷心又憤怒的米高跑去已逝偶像吉他之神的紀念碑內，卻意外進入「夜總會」的世界，米高變成了只能被亡靈看見的「活死人」。米高必須在亡靈節結束前找到在夜總會的祖先家人，才能回到活人的世界。他是否能在夜總會的世界釐清家族對音樂的怨念，並在亡靈節結束找到回家的路呢？</h3>
+    <div class="row mdetails">
+        <div class="col-md-5">
+            <div class="main-title py-3">電影資訊</div>
+            <div class="d-flex flex-row">
+                <div class="d-flex flex-column left">
+                    <div class="py-2">日期</div>
+                    <div class="py-2">導演</div>
+                    <div class="py-2">演員</div>
+                </div>
+                <div class="d-flex flex-column pl-4 right">
+                    <div class="py-2">2017/12/27</div>
+                    <div class="py-2">李·安克里奇(Lee Unkrich)</div>
+                    <div class="py-2">蓋爾·賈西亞·貝納(Gael García Bernal), 本杰明·布拉特(Benjamin Bratt), 芮妮·维克托(Renée Victor)</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-6 trailer">
+            <div class="main-title py-3">電影預告</div>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JmSU0ZjjjXM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-md-12 story">
+            <div class="main-title py-3">劇情介紹</div>
+            <div class="story-body">《可可夜總會》故事描述墨西哥的鞋匠家族「里韋拉斯」有個代代相傳的家訓，就是家族的成員絕不能與音樂有任何關係，但天生音樂魂的男孩米高難以抵擋音樂的魅力，時常躲在家中閣樓練習，更無師自通了一身不得了的吉他技巧。就在「亡靈節」這天，父親發現了充滿樂譜的秘密閣樓，盛怒之下砸了他的吉他，傷心又憤怒的米高跑去已逝偶像吉他之神的紀念碑內，卻意外進入「夜總會」的世界，米高變成了只能被亡靈看見的「活死人」。米高必須在亡靈節結束前找到在夜總會的祖先家人，才能回到活人的世界。他是否能在夜總會的世界釐清家族對音樂的怨念，並在亡靈節結束找到回家的路呢？</div>
+        </div>
     </div>
 </div>
