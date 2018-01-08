5e202c6c54112d7a1dc744efc056110ffdff87c9
BerSiang <vincent0740@gmail.com>
2018-01-08 18:13:30 +0800

    complaint

--- views/client.view.php
+++ views/client.view.php
@@ -4,6 +4,8 @@
 	$dbFactory = new \Data\DataFactory();
 	$db = $dbFactory->getDB();
 	$user_id = $_SESSION['user_id'];
+	$r = $db->query("SELECT account FROM users WHERE id = $user_id");
+	$account = $r->fetch()['account'];
 	$q = $db->query("SELECT * FROM `customer complaints` WHERE user_id = $user_id");
 
 ?>
@@ -13,7 +15,7 @@
 <form  method="post" action="./complaint_post.php" enctype="multipart/form-data">
   <div class="form-group">
     <label for="moviename">會員帳號:</label>
-    <input type="text" class="form-control" name="date" value="<?php echo $_SESSION['user_account'] ?>" readonly="readonly"/>
+    <input type="text" class="form-control" name="date" value="<?php echo $account; ?>" readonly="readonly"/>
   </div>
   <div class="form-group">
     <label for="comment">留言:</label>
