ce1f2e6c7af037cf621771346a20662a26091f1c
Vincent <vincent0740@gmail.com>
2018-01-08 11:30:57 +0800

    fix

--- public/addcart.php
+++ public/addcart.php
@@ -4,15 +4,17 @@ require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'envir
 require_once(__DIR__ . '/../autoload.php');
 session_start();
 use \Data\ShoppingCart;
-echo $_SESSION['user_id'], $_POST['ticket_id'], $_POST['num'];
+//echo $_SESSION['user_id'], $_POST['ticket_id'], $_POST['num'];
 if(isset($_SESSION['user_id'])) {
 	$act = new \Data\ShoppingCart($_SESSION['user_id'], $_POST['ticket_id']);
 	
 	if(strcasecmp($_POST['action'], 'update') == 0) {
 		$act->update($_POST['num']);
+		echo 'V' . $_POST['num'];
 	}
 	else {
 		$act->add($_POST['num']);
+		echo 'S' . $_POST['num'];
 	}
 	if(strcasecmp($_POST['next_action'], 'pay') == 0) {
 		header('Location: ./cart.php');
